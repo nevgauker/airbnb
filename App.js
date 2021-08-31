6fde427d6aa2d68471b48f709cad691cea713d5e
rotem <nevgauker@gmail.com>
2021-09-01 01:41:08 +0300

    posts list

--- App.js
+++ App.js
@@ -14,11 +14,8 @@ import {
 } from 'react-native';
 
 import HomeScreen from './src/screens/Home';
-import Post from './src/components/Post';
-import feed from './assets/data/feed';
 
-
-const post1 = feed[1];
+import SearchResultsScreen from './src/screens/SearchResults';
 
 const App: () => React$Node = () => {
   return (
@@ -26,9 +23,7 @@ const App: () => React$Node = () => {
       <StatusBar barStyle="dark-content"/>
       <SafeAreaView>
         {/* <HomeScreen/> */}
-        <Post
-          post={post1}
-        />
+        <SearchResultsScreen/>
       </SafeAreaView>
     </>
   );
