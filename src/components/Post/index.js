5e0427d2b86016444b1c0834456d2553f40bacec
rotem <nevgauker@gmail.com>
2021-09-01 01:19:23 +0300

    post props

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -11,18 +11,20 @@ import Fontisto from 'react-native-vector-icons/Fontisto';
 import styles from './styles';
 
 const Post = (props) => {
+
+    const post = props.post;
 return (
     <View style={styles.container}>
-       <Image source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg"}} style={styles.image}/>
+       <Image source={{uri: post.image}} style={styles.image}/>
             
-        <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
-        <Text style={styles.description} numberOfLines={2}>Bright room in the heart of the city</Text>
+        <Text style={styles.bedrooms}>{post.bed} bed {post.bedroom} bedroom</Text>
+        <Text style={styles.description} numberOfLines={2}>{post.type}. {post.title}</Text>
         <Text style={styles.prices}>
-            <Text style={styles.oldPrice}>$36</Text>
-            <Text style={styles.price}> $30</Text>
+            <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+            <Text style={styles.price}> ${post.newPrice}</Text>
             / night
         </Text>
-                <Text style={styles.totalPrice}>$234 total</Text>
+                <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
 )
 }
